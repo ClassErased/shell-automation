b5afac6973bf0027073c4490f8e725679295df4e
sudoxey <109528182+sudoxey@users.noreply.github.com>
2022-09-02 15:30:40 +0800

    Update jammy.sh

--- jammy.sh
+++ jammy.sh
@@ -1,31 +1,48 @@
-# // MAKE GNOME (AND PRIVACY) GREAT AGAIN
+#!/bin/bash
 
+# Make GNOME great again (better privacy and mimic macOS, a little)
 gsettings set org.gnome.desktop.interface clock-format '12h'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-blue-dark'
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll 'true'
+gsettings set org.gnome.desktop.privacy hide-identity 'true'
+gsettings set org.gnome.desktop.privacy old-files-age 'uint32 0'
+gsettings set org.gnome.desktop.privacy recent-files-max-age '1'
+gsettings set org.gnome.desktop.privacy remember-app-usage 'false'
 gsettings set org.gnome.desktop.privacy remember-recent-files 'false'
 gsettings set org.gnome.desktop.privacy remove-old-temp-files 'true'
+gsettings set org.gnome.desktop.privacy remove-old-trash-files 'true'
+gsettings set org.gnome.desktop.privacy report-technical-problems 'false'
+gsettings set org.gnome.desktop.privacy show-full-name-in-top-bar 'false'
 gsettings set org.gnome.desktop.session idle-delay '0'
 gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
 gsettings set org.gnome.mutter center-new-windows 'true'
+gsettings set org.gnome.nautilus.preferences show-create-link 'true'
 gsettings set org.gnome.nautilus.preferences show-delete-permanently 'true'
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size '32'
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size '40'
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height 'false'
 gsettings set org.gnome.shell.extensions.dash-to-dock scroll-action 'cycle-windows'
 gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts 'false'
-gsettings set org.gnome.shell.extensions.dash-to-dock show-trash 'false'
 gsettings set org.gnome.shell.extensions.ding show-home 'false'
+gsettings set org.gnome.shell favorite-apps '[
+    "org.gnome.FileRoller.desktop",
+    "org.gnome.Calculator.desktop",
+    "org.gnome.DiskUtility.desktop",
+    "org.gnome.Nautilus.desktop",
+    "google-chrome.desktop",
+    "org.gnome.eog.desktop",
+    "nvidia-settings.desktop",
+    "gnome-control-center.desktop",
+    "software-properties-gtk.desktop",
+    "update-manager.desktop",
+    "org.gnome.Terminal.desktop",
+    "timeshift-gtk.desktop",
+    "code.desktop"
+]'
 
-# // DISABLE OS SELECTION
-
-sudo tee -a '/etc/default/grub' > /dev/null << EOF
-GRUB_RECORDFAIL_TIMEOUT=0
-EOF
-sudo update-grub
-
-# // ADD PADDING TO TERMINAL WINDOW
-
+# Add padding to terminal window
 tee -a "$HOME/.config/gtk-3.0/gtk.css" > /dev/null << EOF
 VteTerminal,
 TerminalScreen,
@@ -35,25 +52,17 @@ vte-terminal {
 }
 EOF
 
-# // F5 TO CLEAR TERMINAL HISTORY AND WINDOW (FOR OCD REMEDY)
+# Increase boot up speed by disabling os selection menu (not recommended for dual boot)
+sudo tee -a '/etc/default/grub' > /dev/null <<< $'\nGRUB_RECORDFAIL_TIMEOUT=0'
+sudo update-grub
 
-tee -a "$HOME/.bashrc" > /dev/null << EOF
-bind '"\e[15~":"history -cw\C-mclear\C-m"'
-export PATH=$PATH:$HOME/.local/bin
-EOF
-source .bashrc
-
-# // F1-12 AS DEFAULT ON KEYCHRON KEYBOARDS (IF DETECTED)
-
+# Set F1-12 as default on Keychron keyboards (if detected)
 if [[ $(sudo lshw 2> /dev/null | grep Keychron) ]]; then
-sudo tee '/etc/modprobe.d/hid_apple.conf' > /dev/null << EOF
-options hid_apple fnmode=2
-EOF
-sudo update-initramfs -u -k all
+    sudo tee '/etc/modprobe.d/hid_apple.conf' > /dev/null <<< 'options hid_apple fnmode=2'
+    sudo update-initramfs -u -k all
 fi
 
-# // PURGE
-
+# Purge
 sudo apt autoremove --purge -y \
     apport \
     gedit \
@@ -72,37 +81,32 @@ sudo apt autoremove --purge -y \
     whoopsie \
     yelp
 
-# // UPDATE
-
-sudo apt-get update > /dev/null
+# Update
+sudo apt update
 sudo apt full-upgrade -y
 
-# // INSTALL NVIDIA DRIVER (IF DETECTED)
-
+# Nvidia driver (if detected)
 if [[ $(sudo lshw -C display 2> /dev/null | grep vendor) =~ NVIDIA ]]; then
     sudo apt install -y nvidia-driver-515
 fi
 
-# // INSTALL RECOMMENDED APPS
-
+# Recommended apps
 sudo apt install -y \
     curl \
     git \
     timeshift
 
-# // INSTALL FLATPAK
-
+# Flatpak
 sudo apt install -y flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# // INSTALL GOOGLE CHROME
-
-wget https://dl.google.com/linux/linux_signing_key.pub -qO - | sudo gpg --dearmor -o /usr/share/keyrings/google.gpg | sudo tee '/etc/apt/sources.list.d/google-chrome.list' > /dev/null <<< 'deb [arch=amd64 signed-by=/usr/share/keyrings/google.gpg] https://dl.google.com/linux/chrome/deb/ stable main' 
-sudo apt-get update > /dev/null
+# Google Chrome
+wget https://dl.google.com/linux/linux_signing_key.pub -qO - | sudo gpg --dearmor -o /usr/share/keyrings/google.gpg
+sudo tee '/etc/apt/sources.list.d/google-chrome.list' > /dev/null <<< 'deb [arch=amd64 signed-by=/usr/share/keyrings/google.gpg] https://dl.google.com/linux/chrome/deb/ stable main'
+sudo apt update
 sudo apt install -y google-chrome-stable
 
-# // INSTALL UNGOOGLED CHROMIUM (FOR BETTER PRIVACY)
-
+# Ungoogled Chromium (better privacy)
 # flatpak install -y --noninteractive flathub com.github.Eloston.UngoogledChromium
 # mkdir -p "$HOME/.var/app/com.github.Eloston.UngoogledChromium/config"
 # tee "$HOME/.var/app/com.github.Eloston.UngoogledChromium/config/chromium-flags.conf" > /dev/null << EOF
@@ -110,10 +114,10 @@ sudo apt install -y google-chrome-stable
 # --enable-features=WebUIDarkMode
 # EOF
 
-# // INSTALL VISUAL STUDIO CODE
-
-wget https://packages.microsoft.com/keys/microsoft.asc -qO - | sudo gpg --dearmor -o /usr/share/keyrings/microsoft.gpg | sudo tee '/etc/apt/sources.list.d/vscode.list' > /dev/null <<< 'deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main'
-sudo apt-get update > /dev/null
+# Visual Studio Code
+wget https://packages.microsoft.com/keys/microsoft.asc -qO - | sudo gpg --dearmor -o /usr/share/keyrings/microsoft.gpg
+sudo tee '/etc/apt/sources.list.d/vscode.list' > /dev/null <<< 'deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main'
+sudo apt update
 sudo apt install -y code
 sudo tee -a '/etc/sysctl.conf' > /dev/null <<< 'fs.inotify.max_user_watches = 524288'
 sudo sysctl -p > /dev/null
@@ -153,8 +157,13 @@ tee "$HOME/.config/Code/User/settings.json" > /dev/null << EOF
 }
 EOF
 
-# // HIDE
+# Clean
+rm -rf .wget-hsts
+sudo apt clean
+sudo apt autoclean &> /dev/null
+sudo apt autoremove --purge -y &> /dev/null
 
+# Hide
 cp \
     '/usr/share/applications/im-config.desktop' \
     '/usr/share/applications/gnome-language-selector.desktop' \
@@ -168,14 +177,5 @@ tee -a \
     "$HOME/.local/share/applications/software-properties-drivers.desktop" \
     <<< 'Hidden=true' > /dev/null
 
-# // CLEAN
-
-sudo apt clean &> /dev/null
-sudo apt autoclean &> /dev/null
-sudo apt autoremove --purge -y &> /dev/null
-
-# // REBOOT
-
-echo -e '\n\e[1;32m// INSTALLATION COMPLETED...\e[0m\n\n\e[1;31m// REBOOTING SYSTEM IN 10 SECONDS...\e[0m\n'
-sleep 10s
+# Reboot
 sudo reboot
